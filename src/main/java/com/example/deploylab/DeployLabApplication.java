b38b047ccd81a85d2b3c6957c58f67ab5998c21b
chatnip <commcencer@gmail.com>
2026-03-23 10:26:11 +0900

    Add spring Boot project files

---
+++ src/main/java/com/example/deploylab/DeployLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.deploylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployLabApplication.class, args);
+	}
+
+}
